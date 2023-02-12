4ef96d92653fa64bcfc77e11ec10185060d20fd0
piaan <VianRiztanta71@gmail.com>
2023-02-13 03:35:18 +0700

    adding repository

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Beli Pulsa Disini Aja!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- FONT -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- BOOTSTRAP ICON -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <!-- NAVBAR START -->
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-sm ">
+        <div class="container">
+            <a class="navbar-brand" href="#">Pulsa<span>Online</span></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse justify-content-end navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#content"><i class="bi bi-people"></i> Tentang Kami</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#footer"><i class="bi bi-telephone"></i> Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="bi bi-person-circle"></i> Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- NAVBAR END -->
+
+    <!-- WELCOME START -->
+    <section id="welcome">
+        <div class="welcome">
+            <div class="container">
+                <div class="row align-items-center pt-5 ">
+                    <div class="col-12 col-md-6">
+                        <h1>Beli Pulsa Aman Dan Layanan Terbaik Di Pulsa<span>Online</span>.</h1>
+                        <a href="{{ route('product.create') }}" class="button">Beli Pulsa</a>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <img src="img/ilus.png" alt="https://icons8.com/illustrations/author/zD2oqC8lLBBA">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- WELCOME END -->
+
+    <!-- CONTENT START -->
+    <section id="content">
+        <div class="content">
+            <div class="container">
+                <div class="content_isi">
+                    <div class="row text-center align-items-center">
+                        <div class="col-12 col-md-4 mb-2">
+                            <h2>135,060+</h3>
+                                <p>Telah melakukan transaksi di Pulsa<span>Online</span>.</p>
+                        </div>
+                        <div class="col-12 col-md-4 mb-2">
+                            <h2>Pembayaran</h3>
+                                <p>Kalian bisa melakukan transaksi dengan metode pembayaran Dana, Ovo, Gopay, Dan lainnya.</p>
+                        </div>
+                        <div class="col-12 col-md-4 mb-2">
+                            <h2>Layanan</h3>
+                                <p>24 Jam kami akan melayani kalian semua.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- CONTENT END -->
+
+    <!-- TENTANG KAMI START -->
+    <section id="tentang">
+        <div class="about">
+            <div class="container">
+                <div class="about_content">
+                    <div class="row ">
+                        <div class="col-12 col-md-6 text-center">
+                            <h1>Pulsa<span>Online</span></h1>
+                            <p>Kami ingin membuat aktifitas kalian menjadi tidak terhambat, dengan adanya Pulsa<span>Online</span> kalian bisa membeli pulsa tanpa harus membuang waktu kalian untuk pergi keluar rumah atau mengganggu waktu jam kerja kalian.
+                            </p>
+                        </div>
+                        <div class="col-12 col-md-6 text-center">
+                            <h1 class="text-center">Services</h1>
+                            <p>Jangan khawatir saat transaksi disini jika anda mengalami masalah, kami akan membatu kalian jika ada permasalahan atau ingin bertanya.</p>
+                            <a href="" class="button text-center">Hubungi Kami</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- TENTANG KAMI END -->
+
+    <!-- CONTACT AND FOOTER START -->
+    <section id="footer">
+        <div class="footer_awal">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6 text-center mt-5">
+                        <h1>Pulsa<span>Online</span></h1>
+                        <hr>
+                        <p>Jl. Soeharto No.95, Kec. Mayangan, Kota Probolinggo Jawa Timur 67212</p>
+                        <hr>
+                        <div class="sosial_media text-center">
+                            <a href="" class="media"><i class="bi bi-envelope-at-fill"></i></a>
+                            <a href="" class="media"><i class="bi bi-instagram"></i></a>
+                            <a href="" class="media"><i class="bi bi-whatsapp"></i></a>
+                            <a href="" class="media"><i class="bi bi-facebook"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 mt-5">
+                        <h1 class="text-center">FAQ</h1>
+                        <hr>
+                        <div class="FAQ mb-5">
+                            <a href="">1. Apa saja Kartu Prabayar yang bisa diisi disini?</a>
+                            <br>
+                            <a href="">2. Metode pembayaran apa saja yang bisa digunakan untuk transaksi?</a>
+                            <br>
+                            <a href="">3. Berapa minimal pembelian pulsa?</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- CONTACT AND FOOTER END -->
+
+    <!-- FOOTER START -->
+    <section id="footers">
+        <footer>
+            <div class="container text-center">
+                <p><i class="bi bi-c-circle"></i> 2023 PulsaOnline | Web Developer | Andika Riztanta Previan.</p>
+            </div>
+        </footer>
+    </section>
+    <!-- FOOTER END -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
